c14167d5020326f04070fe02686ae3a7a489298f
XinAnzzZ <13023195022@163.com>
2018-11-16 13:51:29 +0800

    init

---
+++ src/main/java/com/alibaba/xinan/sirs/SupermarketItemRetrievalSystemApplication.java
@@ -0,0 +1,16 @@
+package com.alibaba.xinan.sirs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author XinAnzzZ
+ * @date 2018/11/13 13:32
+ */
+@SpringBootApplication
+public class SupermarketItemRetrievalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermarketItemRetrievalSystemApplication.class, args);
+    }
+}
---
+++ src/test/java/com/alibaba/xinan/sirs/SupermarketItemRetrievalSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alibaba.xinan.sirs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SupermarketItemRetrievalSystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
