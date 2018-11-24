3dedd7540e26631a4a670c33469408419a11d838
XinAnzzZ <1150989249@qq.com>
2018-11-24 15:37:22 +0800

    MAPPER

--- src/main/java/com/alibaba/xinan/sirs/mapper/BaseMapper.java
+++ src/main/java/com/alibaba/xinan/sirs/mapper/BaseMapper.java
@@ -1,11 +1,39 @@
 package com.alibaba.xinan.sirs.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import java.util.List;
 
 /**
  * @author XinAnzzZ
  * @date 2018/11/22 15:51
  */
-@Mapper
-public interface BaseMapper {
+public interface BaseMapper<T> {
+
+    /**
+     * save one
+     *
+     * @param t the entity
+     */
+    void save(T t);
+
+    /**
+     * delete by id
+     *
+     * @param id the id
+     */
+    void deleteById(int id);
+
+    /**
+     * find by id
+     *
+     * @param id the id
+     * @return the entity
+     */
+    T findById(int id);
+
+    /**
+     * find all
+     *
+     * @return the list
+     */
+    List<T> findAll();
 }
--- src/main/java/com/alibaba/xinan/sirs/mapper/UserMapper.java
+++ src/main/java/com/alibaba/xinan/sirs/mapper/UserMapper.java
@@ -1,8 +1,12 @@
 package com.alibaba.xinan.sirs.mapper;
 
+import com.alibaba.xinan.sirs.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
 /**
  * @author XinAnzzZ
  * @date 2018/11/22 15:51
  */
-public interface UserMapper {
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
 }
